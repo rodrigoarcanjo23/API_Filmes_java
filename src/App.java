8315864886084ea669ee136bfee5c589f55d1db4
rodrigo arcanjo <rodrigoarcanjo690@gmail.com>
2023-03-29 23:18:28 -0300

    updates

---
+++ src/App.java
@@ -0,0 +1,44 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        // fazer uma conexão HTTP e buscar os top 250 series
+        // String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        // exibir e manipular os dados 
+        for (int i = 0; i < 3; i++) {
+
+            Map<String, String> filme = listaDeFilmes.get(i);
+
+
+            System.out.println("\u001b[31m \u001b[44mTítulo:\u001b[m " + filme.get("title"));
+            System.out.println("\u001b[42mURL da imagem:\u001b[m " +filme.get("image"));
+            double classificacao = Double.parseDouble(filme.get("imDbRating"));
+            int numeroEstrelinhas = (int) classificacao;
+            for (int n = 1; n <= numeroEstrelinhas; n++ ) {
+                System.out.print("⭐");
+            }
+            System.out.println("\n");
+
+            
+        }
+    }
+}
